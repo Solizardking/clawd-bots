8a5b57048f0c76ede60ecbdf36972dcb314270b0
solizardking <beetsbyj@gmail.com>
2026-09-09 11:59:08 -0400

    afs

--- site/app/.well-known/assetlinks.json/route.ts
+++
@@ -1,5 +0,0 @@
-import {NextResponse} from 'next/server';
-import {androidAssetLinks} from '@/lib/android-association';
-export function GET(){
-  return NextResponse.json(androidAssetLinks(),{headers:{'content-type':'application/json; charset=utf-8','cache-control':'public, max-age=3600'}});
-}
